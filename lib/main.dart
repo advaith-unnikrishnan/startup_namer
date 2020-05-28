bafbeac99376d1a019d76572f75f0236decb73c2
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-05-28 23:42:08 +0530

    Add a scrolling List view

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final wordPair=WordPair.random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
+      title: 'Startup name Generator',
+      home: RandomWordsState(),
+    );
+  }
+}
+
+class RandomWordsState extends StatefulWidget {
+  @override
+  _RandomWordsStateState createState() => _RandomWordsStateState();
+}
+
+class _RandomWordsStateState extends State<RandomWordsState> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Startup Name Generator'),),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
